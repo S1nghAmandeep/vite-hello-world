7b6fd3d5f3359be187065997a8d4f486a9234c16
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-03 16:18:39 +0100

    added-component-AppHero

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import AppHero from './AppHero.vue'
 
 export default {
   data() {
     return {
       message: 'La mia prima app con Vite!'
-    }
-  }
+    };
+  },
+  components: { AppHero }
 }
 </script>
 
 <template>
   <div>
     {{ message }}
+    <AppHero />
+
   </div>
 </template>
 
@@ -23,9 +27,11 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
