c6239e0d26e69f3b941ad9dec22f2ac0bf318a56
SinghAmandeep <amandeepsingh3095@gmail.com>
2023-11-03 16:18:00 +0100

    added-bonus-file

---
+++ src/AppHero.vue
@@ -0,0 +1,34 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      message: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <img src="./assets/jumbo.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
